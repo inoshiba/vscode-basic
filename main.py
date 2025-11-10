d47950632fa45686db3c8656b20539e288a27cc2
inoshiba <keks22834@gmail.com>
2025-11-10 11:05:04 +0200

    Update and rename index.html to main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from datetime import datetime
 
-date_str = "2024-05-01"
-requested_day = datetime.strptime(date_str, '%Y-%m-%d').date()
-today_date = datetime.today().date()
-diff_day = today_date - requested_day
-print(diff_day.days)
+def days_diff(date_str):
+    try:
+        requested_day = datetime.strptime(date_str, '%Y-%m-%d').date()
+        today_date = datetime.today().date()
+        diff_day = today_date - requested_day
+        return  diff_day.days
+    except ValueError:
+        return"Помилка"
+
 
 import random
-
 
 def get_numbers_ticket(min, max, quantity):
     if min < 1 or max > 1000 or quantity < 1 or quantity > (max - min + 1):
